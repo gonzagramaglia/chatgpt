59b9818060f8d0f7f1d372fabee4b468b6ed8060
Gonza <gonzagramaglia@gmail.com>
2023-06-06 14:46:39 -0300

    Frontend

--- client/script.js
+++ client/script.js
@@ -37,3 +37,54 @@ function generateUniqueId(){
 
   return `id-${timeStamp}-${hexadecimalString}`
 }
+
+function chatStripe(isAi, value, uniqueId){
+  return(
+    `
+      <div class='wrapper ${isAi && 'ai'}'>
+        <div class='chat' >
+          <div class='profile' >
+            <img 
+              src='${isAi ? bot : user}'
+              alt='${isAi ? 'bot' : 'user'}'
+            />
+          </div>
+          <div 
+            class='message' 
+            id=${uniqueId}
+          >
+            ${value}
+          </div>
+        </div>
+      </div>
+    `
+  )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(data);
+
+  //user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  //user's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv)
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13){
+    handleSubmit(e)
+  }
+})
